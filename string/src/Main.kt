a6be92cc9298ba3dc80176f98ba234a39f04a870
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-17 17:57:55 +0300

    ok

---
+++ string/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var name="Yabsera Bogale"
+    println("string length "+ name.length)
+    println("string can be index forexample ${name}: index of 0:${name[0]}")
+    name=name.uppercase()
+    println(name)
+    println(name.lowercase())
+    var one:Int=1
+    println(one.toString().toInt())
+}
