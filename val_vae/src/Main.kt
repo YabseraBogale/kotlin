28f0ce369ab631e407bd6fa3a0bb5d80ed57b52d
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-17 17:29:12 +0300

    ok

---
+++ val_vae/src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// cannot redeclare val
+fun main() {
+    val name="Yabsera Bogale"
+    val name="Yabsera Bogale"
+}
