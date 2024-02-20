3b371c6a76c0affc80b6121b8d21d37b228f35ef
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-20 16:41:33 +0300

    ok

--- string/src/Main.kt
+++ string/src/Main.kt
@@ -1,10 +1,5 @@
 fun main() {
-    var name="Yabsera Bogale"
-    println("string length "+ name.length)
-    println("string can be index forexample ${name}: index of 0:${name[0]}")
-    name=name.uppercase()
-    println(name)
-    println(name.lowercase())
-    var one:Int=1
-    println(one.toString().toInt())
+    val spaces="      "
+    // spaces are counted
+    println(spaces.trim().length!=spaces.length)
 }
