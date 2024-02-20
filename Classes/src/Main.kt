8d4e4a8d942f79e544fb491388c4e73c085472a7
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-02-20 17:22:02 +0300

    when the parameters are val and var it becomes properties. if not written it become normal argument

--- Classes/src/Main.kt
+++ Classes/src/Main.kt
@@ -2,11 +2,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class Person(val name:String){
+    private val firstName:String
     init {
-        require(name.trim().length>0){ "Invalid input"}
+        this.firstName=name
     }
+    fun Name()=this.firstName
 }
 fun main() {
         val yabsera=Person("Yabsera")
+        println(yabsera.Name())
         println(yabsera.name)
 }
